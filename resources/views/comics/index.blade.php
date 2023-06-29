a23845d0f525f8f6d6dc1a50ab2191053e931b7f
Andriy-Kolokolov <koloandre22@gmail.com>
2023-06-29 18:56:49 +0200

    add img folder, scss styles

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.base')
 
 @section('content')
-    <div class="row justify-content-center">
+    <div class="row justify-content-center g-4">
         @foreach ($comics as $comic)
             <div class="col-lg-2 col-md-3 col-sm-4 col-6">
                 <div class="grid-item">
                     <div class="img-wrapper">
                         <img src="{{ $comic->thumb }}" alt="comic image">
                     </div>
-                    <h3>{{ $comic->title }}</h3>
+                    <h5 class="mt-4 text-center">{{ $comic->title }}</h5>
                 </div>
             </div>
         @endforeach
