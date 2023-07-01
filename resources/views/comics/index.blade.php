e84fb7d3d350c9181549931a84dfcaf8e458c3db
Andriy-Kolokolov <koloandre22@gmail.com>
2023-07-01 17:42:27 +0200

    add crud btns

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
     <div class="row justify-content-center g-4 text-white">
+        <div class="text-center fs-2 p-1">
+            <button class="btn btn-primary"><a href="/comix.create" class="text-white fw-bold text-decoration-none">ADD COMIC</a></button>
+        </div>
         @foreach ($comics as $comic)
             <div class="col-lg-2 col-md-3 col-sm-4 col-6">
                 <div class="grid-item">
@@ -9,6 +12,10 @@
                         <img src="{{ $comic->thumb }}" alt="comic image">
                     </div>
                     <h5 class="mt-4 text-center">{{ $comic->title }}</h5>
+                    <div class="d-flex justify-content-around me-auto fs-4">
+                        <a href="/comix.update"><i class="fa-solid fa-pen-to-square"></i></a>
+                        <a href="/comix.delete"><i class="fa-solid fa-trash-can"></i></a>
+                    </div>
                 </div>
             </div>
         @endforeach
